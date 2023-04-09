de66bdad6dca172bbcd70e0869390a9b390503dc
Matheus <matheus53barros@hotmail.com>
2023-04-09 11:26:34 -0400

    Criado query com consulta capaz de exibir somente os funcionários que realizaram mais ou igual a 2 locações.

---
+++ Consulta capaz de exibir somente os funcionários que realizaram mais ou igual a 2 locações.sql
@@ -0,0 +1,9 @@
+-- SQLite
+
+-- Consulta capaz de exibir somente os funcionários que realizaram mais ou igual a 2 locações.
+
+SELECT E.NAME AS 'Nome', E.PHONE_NUMBER AS 'Telefone', E.CONTRACT_DATE AS 'Data do contrato', P.DESCRIPTION AS 'Cargo', COUNT(L.EMPLOYEE_ID) AS 'Quantidade de vendas' FROM EMPLOYEES AS E 
+JOIN LOCATIONS AS L ON(E.ID = L.EMPLOYEE_ID) 
+JOIN POSITIONS AS P ON(E.POSITION_ID = P.ID) 
+GROUP BY L.EMPLOYEE_ID
+HAVING COUNT(NOME) >= 2;
