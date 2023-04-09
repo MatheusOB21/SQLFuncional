df022b685fd040cfed427a75663672d413d45209
Matheus <matheus53barros@hotmail.com>
2023-04-09 10:26:10 -0400

    Criado query para editar o campo email do cliente com nome Carolina

---
+++ Alterando o campo e-mail do cliente com nome de Carolina.sql
@@ -0,0 +1,5 @@
+-- SQLite
+
+--Editando o campo e-mail do cliente com nome Carolina para carolina@campuscode.com.br
+
+UPDATE CUSTOMERS SET EMAIL = 'carolina@campuscode.com.br' WHERE NAME = 'Carolina'; 
